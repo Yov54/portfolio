17e86df237d88ff06958af6e5b11290a31fd3235
Yov54 <yovi.revikasari03@gmail.com>
2026-08-25 02:47:00 +0700

    Prepare portfolio for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfo/",
+  base: "/portfolio/",
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'vendor-react';
-            }
-            return 'vendor-core';
-          }
-        }
-      }
-    },
     chunkSizeWarningLimit: 1000
   }
 })
